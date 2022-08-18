2d0380ec0c4c2587c2b71ab4ff7b50fc26ac64ed
Amani Kashema <amanikashema000@gmail.com>
2022-08-18 16:55:48 +0200

    updated js toggle function

--- app.js
+++ app.js
@@ -2,3 +2,23 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+
+const modalBtn = document.querySelector(".modal-btn")
+const overlay = document.querySelector(".modal-overlay")
+const closeBtn = document.querySelector(".close-btn")
+
+modalBtn.addEventListener("click", function(){
+  
+//   if(overlay.classList.contains("open-modal")){
+//     overlay.classList.remove("open-modal")
+//   }
+//   else{
+//     overlay.classList.add("open-modal")
+//   }
+
+overlay.classList.toggle("open-modal")
+})
+
+closeBtn.addEventListener("click", function(){
+    overlay.classList.remove("open-modal")
+})
